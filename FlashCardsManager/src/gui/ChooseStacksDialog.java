435e216dbca483eafea10327b102086c0bb93741
MissJule <moni.schrenk@gmail.com>
2014-11-19 13:47:15 +0100

    Fixed: no cards in edit flashcards dialog after learning session, caused by random order of flashcards

--- FlashCardsManager/src/gui/ChooseStacksDialog.java
+++ FlashCardsManager/src/gui/ChooseStacksDialog.java
@@ -22,138 +22,145 @@ import core.LearningProject;
 
 public class ChooseStacksDialog extends JDialog {
 
-	MainWindow owner;
-	ArrayList<FlashCard> allCards, sessionCards;
-	private LearningProject project;
+   MainWindow owner;
+   ArrayList<FlashCard> cards, copiedCards, sessionCards;
+   private LearningProject project;
 
-	private JPanel pnlControls;
-	private Box centerBox;
-	private StackBox[] boxes;
-	private JButton btnOk, btnDiscard;
-	private boolean activeSelection;
+   private JPanel pnlControls;
+   private Box centerBox;
+   private StackBox[] boxes;
+   private JButton btnOk, btnDiscard;
+   private boolean activeSelection;
 
-	public ChooseStacksDialog(MainWindow owner, ArrayList<FlashCard> allCards,
-			LearningProject project) throws SQLException {
-		super(owner, false);
-		this.owner = owner;
-		this.allCards = allCards;
-		this.project = project;
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setTitle("Stapelauswahl..");
-		setLayout(new BorderLayout());
+   public ChooseStacksDialog(MainWindow owner, ArrayList<FlashCard> allCards,
+         LearningProject project) throws SQLException {
+      super(owner, false);
+      this.owner = owner;
+      this.project = project;
+      cards = copyCards(allCards);
+      setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+      setTitle("Stapelauswahl..");
+      setLayout(new BorderLayout());
 
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (UnsupportedLookAndFeelException e) {
-			e.printStackTrace();
-		}
+      try {
+         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      } catch (ClassNotFoundException e) {
+         e.printStackTrace();
+      } catch (InstantiationException e) {
+         e.printStackTrace();
+      } catch (IllegalAccessException e) {
+         e.printStackTrace();
+      } catch (UnsupportedLookAndFeelException e) {
+         e.printStackTrace();
+      }
 
-		createWidgets();
-		addWidgets();
-		setListeners();
+      createWidgets();
+      addWidgets();
+      setListeners();
 
-		pack();
-		setLocationRelativeTo(owner);
-	}
-	
-	// CREATE WIDGETS: create GUI components
-	private void createWidgets() throws SQLException {
-		pnlControls = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
-		centerBox = Box.createVerticalBox();
-		boxes = new StackBox[project.getNumberOfStacks()];
-		for (int i = 1; i <= project.getNumberOfStacks(); i++) {
-			boxes[i - 1] = new StackBox(i, project.getNumberOfCards(i));
-		}
-		btnOk = new JButton("Ok");
-		btnDiscard = new JButton("Abbrechen");
+      pack();
+      setLocationRelativeTo(owner);
+   }
+   
+   // CREATE WIDGETS: create GUI components
+   private void createWidgets() throws SQLException {
+      pnlControls = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+      centerBox = Box.createVerticalBox();
+      boxes = new StackBox[project.getNumberOfStacks()];
+      for (int i = 1; i <= project.getNumberOfStacks(); i++) {
+         boxes[i - 1] = new StackBox(i, project.getNumberOfCards(i));
+      }
+      btnOk = new JButton("Ok");
+      btnDiscard = new JButton("Abbrechen");
 
-	}
-	
-	// ADD WIDGETS: put the GUI together
-	private void addWidgets() {
-		this.add(centerBox, BorderLayout.CENTER);
-		this.add(pnlControls, BorderLayout.SOUTH);
-		for (int i = 1; i <= project.getNumberOfStacks(); i++) {
-			centerBox.add(boxes[i - 1]);
-		}
-		pnlControls.add(btnDiscard);
-		pnlControls.add(btnOk);
-	}
-	
-	// SET LISTENERS: add listeners to GUI elements
-	private void setListeners() {
-		btnDiscard.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				ChooseStacksDialog.this.dispose();
-			}
-		});
+   }
+   
+   // ADD WIDGETS: put the GUI together
+   private void addWidgets() {
+      this.add(centerBox, BorderLayout.CENTER);
+      this.add(pnlControls, BorderLayout.SOUTH);
+      for (int i = 1; i <= project.getNumberOfStacks(); i++) {
+         centerBox.add(boxes[i - 1]);
+      }
+      pnlControls.add(btnDiscard);
+      pnlControls.add(btnOk);
+   }
+   
+   // SET LISTENERS: add listeners to GUI elements
+   private void setListeners() {
+      btnDiscard.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            ChooseStacksDialog.this.dispose();
+         }
+      });
 
-		btnOk.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				sessionCards = constructRandomSession();
-				LearningSession newSession = new LearningSession(
-						ChooseStacksDialog.this.owner,
-						ChooseStacksDialog.this.project,
-						ChooseStacksDialog.this.sessionCards);
-				if (activeSelection) {
-					newSession.setVisible(true);
-					ChooseStacksDialog.this.dispose();
-				} else {
-					JOptionPane.showMessageDialog(ChooseStacksDialog.this,
-							"Bitte Stapel ausw\u00e4hlen!", "Achtung",
-							JOptionPane.WARNING_MESSAGE);
-				}
-			}
-		});
-	}
-	
-	
-	// CONSTRUCT SESSION: onstructs session with cards from selected stacks in order from database entries
-	private ArrayList<FlashCard> constructSession() {
-		sessionCards = new ArrayList<FlashCard>();
-		for (int i = 0; i < boxes.length; i++) {
-			if (boxes[i].isSelected()) {
-				activeSelection = true; // for error msg when no selection
-				ListIterator<FlashCard> lit = allCards.listIterator();
-				while (lit.hasNext()) {
-					FlashCard f = lit.next();
-					if (f.getStack() == i + 1) { // stacks go from 1 upwards
-						sessionCards.add(f);
-						System.out.println("Copied card " + f.getId()
-								+ " (Stack " + f.getStack() + ")...");
-					}
-				}
-			}
-		}
-		return sessionCards;
-	}
-	
-	// CONSTRUCT RANDOM SESSION: constructs session with cards from selected stacks in random order
-	private ArrayList<FlashCard> constructRandomSession() {
-		sessionCards = new ArrayList<FlashCard>();
-		Random random = new Random();
-		while (!allCards.isEmpty()) { 
-			int randomIndex = random.nextInt(allCards.size()); 
-			FlashCard currentCard = allCards.get(randomIndex);
-			allCards.remove(currentCard); // anyway remove card from project arraylist
-			int stack = currentCard.getStack()-1; //stack numbers are from 1 upwards
-			System.out.println(stack);
-			if (boxes[stack].isSelected()) { // get only cards from selected stacks
-				activeSelection = true; // for error msg when no selection
-				sessionCards.add(currentCard); 
-				System.out.println("Copied card " + currentCard.getId()
-						+ " (Stack " + currentCard.getStack() + ")...");
-			}
-		}
-		return sessionCards;
-	}
+      btnOk.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            sessionCards = constructRandomSession();
+            LearningSession newSession = new LearningSession(
+                  ChooseStacksDialog.this.owner,
+                  ChooseStacksDialog.this.project,
+                  ChooseStacksDialog.this.sessionCards);
+            if (activeSelection) {
+               newSession.setVisible(true);
+               ChooseStacksDialog.this.dispose();
+            } else {
+               JOptionPane.showMessageDialog(ChooseStacksDialog.this,
+                     "Bitte Stapel ausw\u00e4hlen!", "Achtung",
+                     JOptionPane.WARNING_MESSAGE);
+            }
+         }
+      });
+   }
+   
+   // COPY CARDS: copy flashcard array from Project Panel, because the cards will be deleted
+   // while constructing an empty session, and are needed e.g. in LearningSession
+   private ArrayList<FlashCard> copyCards(ArrayList<FlashCard> srcCards) {
+      ArrayList<FlashCard> targetCards = new ArrayList<FlashCard>();
+      targetCards.addAll(srcCards);
+      return targetCards;
+   }
+   
+   // CONSTRUCT SESSION: constructs session with cards from selected stacks in order from database entries
+   private ArrayList<FlashCard> constructSession() {
+      sessionCards = new ArrayList<FlashCard>();
+      for (int i = 0; i < boxes.length; i++) {
+         if (boxes[i].isSelected()) {
+            activeSelection = true; // for error msg when no selection
+            ListIterator<FlashCard> lit = cards.listIterator();
+            while (lit.hasNext()) {
+               FlashCard f = lit.next();
+               if (f.getStack() == i + 1) { // stacks go from 1 upwards
+                  sessionCards.add(f);
+                  System.out.println("Copied card " + f.getId()
+                        + " (Stack " + f.getStack() + ")...");
+               }
+            }
+         }
+      }
+      return sessionCards;
+   }
+   
+   // CONSTRUCT RANDOM SESSION: constructs session with cards from selected stacks in random order
+   private ArrayList<FlashCard> constructRandomSession() {
+      sessionCards = new ArrayList<FlashCard>();
+      Random random = new Random();
+      while (!cards.isEmpty()) { 
+         int randomIndex = random.nextInt(cards.size()); 
+         FlashCard currentCard = cards.get(randomIndex);
+         cards.remove(currentCard); // anyway remove card from project arraylist
+         int stack = currentCard.getStack()-1; //stack numbers are from 1 upwards
+         System.out.println(stack);
+         if (boxes[stack].isSelected()) { // get only cards from selected stacks
+            activeSelection = true; // for error msg when no selection
+            sessionCards.add(currentCard); 
+            System.out.println("Copied card " + currentCard.getId()
+                  + " (Stack " + currentCard.getStack() + ")...");
+         }
+      }
+      return sessionCards;
+   }
 
 }
--- FlashCardsManager/src/gui/FlashCardPanel.java
+++ FlashCardsManager/src/gui/FlashCardPanel.java
@@ -14,6 +14,7 @@ import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -23,175 +24,178 @@ import exc.EntryNotFoundException;
 
 public class FlashCardPanel extends JPanel {
 
-	static BufferedImage imgShowMore, imgEdit, imgDelete, imgRed, imgYellow,
-			imgGreen;
+   static BufferedImage imgShowMore, imgEdit, imgDelete, imgRed, imgYellow,
+         imgGreen;
 
-	static {
-		try {
-			// TODO: design imgShowMore
-			imgEdit = ImageIO.read(ProjectPanel.class.getClassLoader()
-					.getResourceAsStream("img/ImgEdit_16x16.png"));
-			imgDelete = ImageIO.read(ProjectPanel.class.getClassLoader()
-					.getResourceAsStream("img/ImgDelete_16x16.png"));
-			imgRed = ImageIO.read(ProjectPanel.class.getClassLoader()
-					.getResourceAsStream("img/ImgRed_8x8.png"));
-			imgYellow = ImageIO.read(ProjectPanel.class.getClassLoader()
-					.getResourceAsStream("img/ImgYellow_8x8.png"));
-			imgGreen = ImageIO.read(ProjectPanel.class.getClassLoader()
-					.getResourceAsStream("img/ImgGreen_8x8.png"));
+   static {
+      try {
+         // TODO: design imgShowMore
+         imgEdit = ImageIO.read(ProjectPanel.class.getClassLoader()
+               .getResourceAsStream("img/ImgEdit_16x16.png"));
+         imgDelete = ImageIO.read(ProjectPanel.class.getClassLoader()
+               .getResourceAsStream("img/ImgDelete_16x16.png"));
+         imgRed = ImageIO.read(ProjectPanel.class.getClassLoader()
+               .getResourceAsStream("img/ImgRed_8x8.png"));
+         imgYellow = ImageIO.read(ProjectPanel.class.getClassLoader()
+               .getResourceAsStream("img/ImgYellow_8x8.png"));
+         imgGreen = ImageIO.read(ProjectPanel.class.getClassLoader()
+               .getResourceAsStream("img/ImgGreen_8x8.png"));
 
-		} catch (IOException e) {
-			System.out.println("Picture not found");
-			// TODO: JDialog mit ErrorMsg
-		}
-	}
+      } catch (IOException e) {
+         System.out.println("Picture not found");
+         // TODO: JDialog mit ErrorMsg
+      }
+   }
 
-	private Box b;
-	JLabel lblStatus, lblText;
-	private JButton btnShowMore, btnEdit, btnDelete;
-	private Status status;
-	private FlashCard card;
-	private LearningProject project;
-	private ProjectPanel projectPnl;
-	EditFlashcardsDialog editDialog;
+   private Box b;
+   JLabel lblStatus, lblText;
+   private JButton btnShowMore, btnEdit, btnDelete;
+   private Status status;
+   // private JCheckBox chk;
+   private FlashCard card;
+   private LearningProject project;
+   private ProjectPanel projectPnl;
+   EditFlashcardsDialog editDialog;
 
-	public FlashCardPanel(FlashCard card, LearningProject project,
-			Status status, ProjectPanel projectPnl,
-			EditFlashcardsDialog editDialog) {
-		this.status = status;
-		this.card = card;
-		this.projectPnl = projectPnl;
-		this.editDialog = editDialog;
-		this.project = project;
-		this.setLayout(new BorderLayout());
-		createWidgets();
-		addWidgets();
-		setListeners();
-	}
+   public FlashCardPanel(FlashCard card, LearningProject project,
+         Status status, ProjectPanel projectPnl,
+         EditFlashcardsDialog editDialog) {
+      this.status = status;
+      this.card = card;
+      this.projectPnl = projectPnl;
+      this.editDialog = editDialog;
+      this.project = project;
+      this.setLayout(new BorderLayout());
+      createWidgets();
+      addWidgets();
+      setListeners();
+   }
 
-	private void createWidgets() {
-		b = Box.createHorizontalBox();
-		b.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		setStatus(this.status);
+   private void createWidgets() {
+      b = Box.createHorizontalBox();
+      b.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+      setStatus(this.status);
+      // chk = new JCheckBox();
+       lblText = new JLabel("Frage " + card.getId() + ": " + getQuestionTitle(card));
+      lblText.setFont(lblText.getFont().deriveFont(Font.BOLD, 12));
+      btnEdit = new JButton(new ImageIcon(imgEdit));
+      btnEdit.setToolTipText("FlashCard bearbeiten");
+      btnDelete = new JButton(new ImageIcon(imgDelete));
+      btnDelete.setToolTipText("FlashCard l\u00f6schen");
+   }
 
-		lblText = new JLabel("Frage " + card.getId() + ": " + getQuestionTitle(card)); 
-		lblText.setFont(lblText.getFont().deriveFont(Font.BOLD, 12));
-		// btnShowMore = new JButton(">");
-		// btnShowMore.setToolTipText("mehr..");
-		btnEdit = new JButton(new ImageIcon(imgEdit));
-		btnEdit.setToolTipText("FlashCard bearbeiten");
-		btnDelete = new JButton(new ImageIcon(imgDelete));
-		btnDelete.setToolTipText("FlashCard l\u00f6schen");
-	}
+   private void addWidgets() {
+      this.add(b, BorderLayout.CENTER);
+      b.add(lblStatus);
+      b.add(Box.createRigidArea(new Dimension(15, 0)));
+      b.add(lblText);
+      b.add(Box.createHorizontalGlue());
 
-	private void addWidgets() {
-		this.add(b, BorderLayout.CENTER);
-		b.add(lblStatus);
-		b.add(Box.createRigidArea(new Dimension(15, 0)));
-		b.add(lblText);
-		b.add(Box.createHorizontalGlue());
-		// b.add(Box.createRigidArea(new Dimension(30, 0)));
-		// b.add(btnShowMore);
-		b.add(Box.createRigidArea(new Dimension(15, 0)));
-		b.add(btnEdit);
-		b.add(Box.createRigidArea(new Dimension(15, 0)));
-		b.add(btnDelete);
-	}
-	
-	private String getQuestionTitle(FlashCard f) {
-		String question = f.getQuestion();
-		String result;
-		String [] parts = question.split(" ");
-		if (parts[0].length() > 50) {
-			result = parts[0].substring(0, 40) + "...";
-		} else if (parts.length < 2 || parts[0].length() + parts[1].length() > 50) {
-			result = parts[0] + "...";
-		} else if (parts.length < 3 || parts[0].length() + parts[1].length() + parts[2].length() > 50) {
-			result = parts[0] + " " + parts[1] + "...";
-		} else {
-			result = parts[0] + " " + parts[1] + " " + parts[2] + "...";
-		}
-		return result;	
-	}
 
-	private void setListeners() {
-		
-		btnDelete.addActionListener(new ActionListener() {
+      
+      b.add(Box.createRigidArea(new Dimension(15, 0)));
+      b.add(btnEdit);
+      b.add(Box.createRigidArea(new Dimension(15, 0)));
+      b.add(btnDelete);
+      b.add(Box.createRigidArea(new Dimension(15, 0)));
+      // b.add(chk);
+   }
+   
+   private String getQuestionTitle(FlashCard f) {
+      String question = f.getQuestion();
+      String result;
+      String[] parts = question.split(" ");
+      if (parts[0].length() > 50) {
+         result = parts[0].substring(0, 40) + "...";
+      } else if (parts.length < 2 || parts[0].length() + parts[1].length() > 50) {
+         result = parts[0] + "...";
+      } else if (parts.length < 3
+            || parts[0].length() + parts[1].length() + parts[2].length() > 50) {
+         result = parts[0] + " " + parts[1] + "...";
+      } else {
+         result = parts[0] + " " + parts[1] + " " + parts[2] + "...";
+      }
+      return result;
+   }
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO: remove item in database
-				final OkOrDisposeDialog d = new OkOrDisposeDialog(editDialog.getOwner(), 300, 150);
-				d.setTitle("Wirklich l\u00f6schen?");
-				d.setText("<html>Wirklich die Lernkarte l\u00f6schen? <br>"
-						+ "der Eintrag wird damit vollst\u00e4ndig entfernt.</html>");
-				d.addOkAction(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						try {
-							project.removeCard(FlashCardPanel.this.card);
-							projectPnl.removeCard(card);
-							editDialog.cardPnls.remove(FlashCardPanel.this);
-							editDialog.pnlCenter.remove(editDialog.centerBox);
-							editDialog.centerBox = Box.createVerticalBox();
-							editDialog.addCardsToEditPanel();
-							editDialog.pnlCenter.add(editDialog.centerBox, BorderLayout.NORTH);
-							projectPnl.getOwner().updateProjectStatus(project);
-							editDialog.repaint();
-							editDialog.revalidate();
-						} catch (EntryNotFoundException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						} catch (SQLException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-						d.dispose();
-					}
-				});
-				d.setVisible(true);
+   private void setListeners() {
+      
+      btnDelete.addActionListener(new ActionListener() {
 
-			}
-		});
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            // TODO: remove item in database
+            final OkOrDisposeDialog d = new OkOrDisposeDialog(editDialog.getOwner(), 300, 150);
+            d.setTitle("Wirklich l\u00f6schen?");
+            d.setText("<html>Wirklich die Lernkarte l\u00f6schen? <br>"
+                  + "der Eintrag wird damit vollst\u00e4ndig entfernt.</html>");
+            d.addOkAction(new ActionListener() {
+               @Override
+               public void actionPerformed(ActionEvent e) {
+                  try {
+                     project.removeCard(FlashCardPanel.this.card);
+                     projectPnl.removeCard(card);
+                     editDialog.cardPnls.remove(FlashCardPanel.this);
+                     editDialog.pnlCenter.remove(editDialog.centerBox);
+                     editDialog.centerBox = Box.createVerticalBox();
+                     editDialog.addCardsToEditPanel();
+                     editDialog.pnlCenter.add(editDialog.centerBox, BorderLayout.NORTH);
+                     projectPnl.getOwner().updateProjectStatus(project);
+                     editDialog.repaint();
+                     editDialog.revalidate();
+                  } catch (EntryNotFoundException e1) {
+                     // TODO Auto-generated catch block
+                     e1.printStackTrace();
+                  } catch (SQLException e1) {
+                     // TODO Auto-generated catch block
+                     e1.printStackTrace();
+                  }
+                  d.dispose();
+               }
+            });
+            d.setVisible(true);
 
-		btnEdit.addActionListener(new ActionListener() {
+         }
+      });
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				ChangeFlashcardDialog cfd = new ChangeFlashcardDialog(editDialog, project, projectPnl, card);
-				cfd.setVisible(true);
-			}
+      btnEdit.addActionListener(new ActionListener() {
 
-		});
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            ChangeFlashcardDialog cfd = new ChangeFlashcardDialog(editDialog, project, projectPnl, card);
+            cfd.setVisible(true);
+         }
 
-	}
+      });
 
-	public void changeStatus(Status s) {
-		remove(b);
-		b = Box.createHorizontalBox();
-		b.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		setStatus(s);
-		addWidgets();
-		revalidate();
-	}
+   }
 
-	private void setStatus(Status status) {
-		switch (status) {
-		case RED:
-			lblStatus = new JLabel(new ImageIcon(imgRed));
-			lblStatus
-					.setToolTipText("Los geht's! Diese Kart liegt noch im ersten Stapel");
-			break;
-		case YELLOW:
-			lblStatus = new JLabel(new ImageIcon(imgYellow));
-			lblStatus
-					.setToolTipText("Weiter so! - Diese Karte wurde mindestens 1mal richtig beantwortet");
-			break;
-		case GREEN:
-			lblStatus = new JLabel(new ImageIcon(imgGreen));
-			lblStatus
-					.setToolTipText("Bravo! Diese Karte liegt schon im letzten Stapel");
-		}
-	}
+   public void changeStatus(Status s) {
+      remove(b);
+      b = Box.createHorizontalBox();
+      b.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+      setStatus(s);
+      addWidgets();
+      revalidate();
+   }
+
+   private void setStatus(Status status) {
+      switch (status) {
+      case RED:
+         lblStatus = new JLabel(new ImageIcon(imgRed));
+         lblStatus
+               .setToolTipText("Los geht's! Diese Kart liegt noch im ersten Stapel");
+         break;
+      case YELLOW:
+         lblStatus = new JLabel(new ImageIcon(imgYellow));
+         lblStatus
+               .setToolTipText("Weiter so! - Diese Karte wurde mindestens 1mal richtig beantwortet");
+         break;
+      case GREEN:
+         lblStatus = new JLabel(new ImageIcon(imgGreen));
+         lblStatus
+               .setToolTipText("Bravo! Diese Karte liegt schon im letzten Stapel");
+      }
+   }
 
 }
